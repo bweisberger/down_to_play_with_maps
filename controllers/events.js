dc379bc8ac3c93d80c49b41ea0730896054a4660
William Weisberger <bill.weisberger@gmail.com>
2019-09-25 12:19:51 -0600

    added map to new page

--- controllers/events.js
+++ controllers/events.js
@@ -1,182 +1,187 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/users');
-const Event = require('../models/events');
+const User = require("../models/users");
+const Event = require("../models/events");
 
-router.get('/', async (req, res) => {
-    const colorString = 'Vector-Lovers'
-    const colors = [colorString+'-1-hex',colorString+'-2-hex',colorString+'-3-hex',colorString+'-4-hex',colorString+'-5-hex'];
-    Event.find({}, (err, foundEvents) => {
-      if(err){
-        res.send(err);
-      } else {
-        // console.log(foundEvents)
-        // we can make folders in our views
-        // to seperate each resource
-        res.render('events/index.ejs', {
-          events: foundEvents,
-          colors: colors
-        });
-      }
-    })
-  
-  });
-
-router.get('/new', async (req, res) => {
-    try{
-      //why does this work!?!? Without the line below, error thrown when rendering events/new.ejs
-      //error says "currentUserName is not defined."
-      //With currentUserName commented out, just creating "user" below still works!?!?!
-      const user = await User.findById(req.session.userId);
-      if(req.session.userId){
-        res.render('events/new.ejs', {
-          // currentUserName: user.name
-        });
-      } else {
-      /// ***ALERT***
-      req.session.message = "Please login to host a game."
-      res.redirect('/users/new');
-      }
-    } catch(err){
-      console.log(err, "<--error in new events route");
+router.get("/", async (req, res) => {
+  const colorString = "Vector-Lovers";
+  const colors = [
+    colorString + "-1-hex",
+    colorString + "-2-hex",
+    colorString + "-3-hex",
+    colorString + "-4-hex",
+    colorString + "-5-hex"
+  ];
+  Event.find({}, (err, foundEvents) => {
+    if (err) {
       res.send(err);
-    }    
-});
-router.get('/:id/edit', async (req, res) => {
-    try{
-      if(!req.session.userId){
-        req.session.message="Login to edit an event."
-        res.redirect('/users/login')
-      }
-      const foundEvent = await Event.findById(req.params.id);
-      if(req.session.userId == foundEvent.host._id.toString()){
-        // console.log(foundEvent, "<---- inside edit route, foundEvent");
-        res.render('events/edit.ejs', {event: foundEvent});
-      } 
-      else {
-        req.session.message = "Only the event host may edit this event.";
-        res.redirect('/events/'+req.params.id);
-      }
-    } catch(err){
-          console.log(err, "error in events edit route");
-          res.send(err);
+    } else {
+      // console.log(foundEvents)
+      // we can make folders in our views
+      // to seperate each resource
+      res.render("events/index.ejs", {
+        events: foundEvents,
+        colors: colors
+      });
     }
-});    
+  });
+});
+
+router.get("/new", async (req, res) => {
+  try {
+    //why does this work!?!? Without the line below, error thrown when rendering events/new.ejs
+    //error says "currentUserName is not defined."
+    //With currentUserName commented out, just creating "user" below still works!?!?!
+    const user = await User.findById(req.session.userId);
+    if (req.session.userId) {
+      res.render("events/new.ejs", {
+        // currentUserName: user.name
+      });
+    } else {
+      /// ***ALERT***
+      req.session.message = "Please login to host a game.";
+      res.redirect("/users/new");
+    }
+  } catch (err) {
+    console.log(err, "<--error in new events route");
+    res.send(err);
+  }
+});
+router.get("/:id/edit", async (req, res) => {
+  try {
+    if (!req.session.userId) {
+      req.session.message = "Login to edit an event.";
+      res.redirect("/users/login");
+    }
+    const foundEvent = await Event.findById(req.params.id);
+    if (req.session.userId == foundEvent.host._id.toString()) {
+      // console.log(foundEvent, "<---- inside edit route, foundEvent");
+      res.render("events/edit.ejs", { event: foundEvent });
+    } else {
+      req.session.message = "Only the event host may edit this event.";
+      res.redirect("/events/" + req.params.id);
+    }
+  } catch (err) {
+    console.log(err, "error in events edit route");
+    res.send(err);
+  }
+});
 
 //post route to add event id to logged in user
-router.post('/:id', async (req,res)=>{
-      try{
-          const foundEvent = await Event.findById(req.params.id);
-        if(req.session.userId){ 
-          if(foundEvent.availableSpots){
-            const foundUser = await User.findById(req.session.userId);
-            foundUser.event = req.params.id;
-            foundUser.save();
-            req.session.message = "You have been added to the event."
-            // console.log(foundUser);
-            const foundPlayers = await User.find({event:req.params.id});
-            console.log(foundEvent.maxNumberOfPlayers, "<--maxplayers");
-            console.log(foundPlayers.length, "<--foundplayers.length");
-            console.log(foundEvent.maxNumberOfPlayers, "<--maxplayers");
+router.post("/:id", async (req, res) => {
+  try {
+    const foundEvent = await Event.findById(req.params.id);
+    if (req.session.userId) {
+      if (foundEvent.availableSpots) {
+        const foundUser = await User.findById(req.session.userId);
+        foundUser.event = req.params.id;
+        foundUser.save();
+        req.session.message = "You have been added to the event.";
+        // console.log(foundUser);
+        const foundPlayers = await User.find({ event: req.params.id });
+        console.log(foundEvent.maxNumberOfPlayers, "<--maxplayers");
+        console.log(foundPlayers.length, "<--foundplayers.length");
+        console.log(foundEvent.maxNumberOfPlayers, "<--maxplayers");
 
-            const newAvailableSpots = foundEvent.maxNumberOfPlayers - foundPlayers.length;
-            foundEvent.availableSpots = newAvailableSpots;
-            console.log(foundEvent.availableSports, "<--availableSpots");
-            foundEvent.save();
-            // console.log(foundEvent);
-            res.redirect("/events/" + req.params.id);
-          } else {
-            req.session.message = "This game is full."
-            res.redirect('/events/'+req.params.id);
-        } 
-      } else{
-          req.session.message = "You must login to join a game."
-          res.redirect('/users/login')
-      }
-        // console.log(req.session, 'this is the req.session'
-    }catch(err){
-      console.log(err);
-      res.send(err);
-    }
-});
-  
-  router.get('/:id', async (req, res) => {
-    try{
-      const foundEvent = await Event.findById(req.params.id).populate('host');
-      // console.log(foundEvent, " <--- found event in show route");
-      const foundUser = await User.findById(req.session.userId);
-      // console.log(foundUser, " <--- found user in show route");
-      const foundPlayers = await User.find({event:req.params.id});
-      const newAvailableSpots = foundEvent.maxNumberOfPlayers - foundPlayers.length;
-            foundEvent.availableSpots = newAvailableSpots;
-            console.log(foundEvent.availableSports, "<--availableSpots");
-            foundEvent.save();
-      // console.log(foundPlayers, " <--- found players in show route");
-      // console.log(foundUser._id.toString() == foundEvent.host._id.toString(), "<--foundUser._id == foundEvent._host.id")
-      res.render('events/show.ejs', {
-          event: foundEvent,
-          user: foundUser,
-          players: foundPlayers
-        });
-      } catch(err){
-        console.log(err);
-        res.send(err);
-      }
-  });
-
-  //post route to create new event
-  router.post('/', async (req, res) => {
-    // req.body is the information from the form
-    try{
-      // console.log(req.body, ' req.body in post route')
-      // console.log(req.session.userId, 'req.session.userId')
-      req.body.host = req.session.userId;
-      // console.log(req.body.host, " req.body.host in post route")
-      const createdEvent = await Event.create(req.body);
-      createdEvent.populate('host');
-      // console.log(createdEvent, ' < createdEvent in post route');
-      res.redirect('/events/' + createdEvent._id)
-    } catch(err){
-      console.log(err, "error in post route to make new game");
-      res.send(err);
-    }  
-});
-
-  
-  router.put('/:id', (req, res) => {
-    // console.log(req.params, " params in the show route")
-    Event.findByIdAndUpdate(req.params.id, req.body, (err, updatedEvent) => {
-      if(err){
-        res.send(err);
+        const newAvailableSpots =
+          foundEvent.maxNumberOfPlayers - foundPlayers.length;
+        foundEvent.availableSpots = newAvailableSpots;
+        console.log(foundEvent.availableSports, "<--availableSpots");
+        foundEvent.save();
+        // console.log(foundEvent);
+        res.redirect("/events/" + req.params.id);
       } else {
-        console.log(updatedEvent, ' <-- show route document from model');
-        res.redirect('/events/'+req.params.id);  
-      };
-    });
-  });
-  
-router.delete('/:id', async (req, res) => {
-    try{
-        console.log('in delete route');
-        const foundEvent = await Event.findById(req.params.id).populate('host');
-        const users = await User.find({event: req.params.id}).populate('event');
-        if(req.session.userId == foundEvent.host._id.toString()){
-            const deletedEvent = await foundEvent.remove();
-            // console.log(deletedEvent, "<----deletedEvent in delete route");
-            req.session.message = "Event Deleted"
-            users.forEach(function(user){
-                user.event = null;
-            });
-            res.redirect('/events/');
-        } 
-        else {
-            req.session.message = "Only the host may delete this event"
-        }
-        res.redirect('/events/' + req.params.id);
-    } catch (err){
-        console.log(err);
-        res.send(err);
+        req.session.message = "This game is full.";
+        res.redirect("/events/" + req.params.id);
+      }
+    } else {
+      req.session.message = "You must login to join a game.";
+      res.redirect("/users/login");
     }
+    // console.log(req.session, 'this is the req.session'
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
 });
-  
+
+router.get("/:id", async (req, res) => {
+  try {
+    const foundEvent = await Event.findById(req.params.id).populate("host");
+    // console.log(foundEvent, " <--- found event in show route");
+    const foundUser = await User.findById(req.session.userId);
+    // console.log(foundUser, " <--- found user in show route");
+    const foundPlayers = await User.find({ event: req.params.id });
+    const newAvailableSpots =
+      foundEvent.maxNumberOfPlayers - foundPlayers.length;
+    foundEvent.availableSpots = newAvailableSpots;
+    console.log(foundEvent.availableSports, "<--availableSpots");
+    foundEvent.save();
+    // console.log(foundPlayers, " <--- found players in show route");
+    // console.log(foundUser._id.toString() == foundEvent.host._id.toString(), "<--foundUser._id == foundEvent._host.id")
+    res.render("events/show.ejs", {
+      event: foundEvent,
+      user: foundUser,
+      players: foundPlayers
+    });
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
+//post route to create new event
+router.post("/", async (req, res) => {
+  // req.body is the information from the form
+  try {
+    // console.log(req.body, ' req.body in post route')
+    // console.log(req.session.userId, 'req.session.userId')
+    req.body.host = req.session.userId;
+    console.log(req.body, "req.body in post route for events");
+    // console.log(req.body.host, " req.body.host in post route")
+    const createdEvent = await Event.create(req.body);
+    createdEvent.populate("host");
+    // console.log(createdEvent, ' < createdEvent in post route');
+    res.redirect("/events/" + createdEvent._id);
+  } catch (err) {
+    console.log(err, "error in post route to make new game");
+    res.send(err);
+  }
+});
+
+router.put("/:id", (req, res) => {
+  // console.log(req.params, " params in the show route")
+  Event.findByIdAndUpdate(req.params.id, req.body, (err, updatedEvent) => {
+    if (err) {
+      res.send(err);
+    } else {
+      console.log(updatedEvent, " <-- show route document from model");
+      res.redirect("/events/" + req.params.id);
+    }
+  });
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    console.log("in delete route");
+    const foundEvent = await Event.findById(req.params.id).populate("host");
+    const users = await User.find({ event: req.params.id }).populate("event");
+    if (req.session.userId == foundEvent.host._id.toString()) {
+      const deletedEvent = await foundEvent.remove();
+      // console.log(deletedEvent, "<----deletedEvent in delete route");
+      req.session.message = "Event Deleted";
+      users.forEach(function(user) {
+        user.event = null;
+      });
+      res.redirect("/events/");
+    } else {
+      req.session.message = "Only the host may delete this event";
+    }
+    res.redirect("/events/" + req.params.id);
+  } catch (err) {
+    console.log(err);
+    res.send(err);
+  }
+});
+
 module.exports = router;
